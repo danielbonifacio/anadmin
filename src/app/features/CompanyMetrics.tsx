0d192c5caa2a48a298af27b6409427f3b7f95e71
Daniel Bonifacio <contato@danielbonifacio.com.br>
2021-07-24 16:46:06 -0300

    13.14 - Configurando label do eixo horizontal

--- src/app/features/CompanyMetrics.tsx
+++ src/app/features/CompanyMetrics.tsx
@@ -1,5 +1,6 @@
 import { Area, AreaConfig } from '@ant-design/charts';
 import { MetricService } from 'danielbonifacio-sdk';
+import { format } from 'date-fns';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import transformDataIntoAntdChart from '../../core/utils/transformDataIntoAntdChart';
@@ -33,6 +34,13 @@ export default function CompanyMetrics() {
           return legend === 'totalRevenues'
             ? 'Receitas'
             : 'Despesas';
+        },
+      },
+    },
+    xAxis: {
+      label: {
+        formatter(item) {
+          return format(new Date(item), 'MM/yyyy');
         },
       },
     },
