ce0e535205bad3f2e7f774bdc098c3967ef3c210
Daniel Bonifacio <contato@danielbonifacio.com.br>
2021-07-25 03:20:33 -0300

    13.32 - Delimitando e centralizando o Layout

--- src/app/layouts/Default/Header.tsx
+++ src/app/layouts/Default/Header.tsx
@@ -9,7 +9,11 @@ export default function DefaultLayoutHeader() {
     <Header className='header'>
       <Row
         justify='space-between'
-        style={{ height: '100%' }}
+        style={{
+          height: '100%',
+          maxWidth: 1190,
+          margin: '0 auto',
+        }}
         align='middle'
       >
         <img src={logo} alt={'AlgaNews Admin'} />
