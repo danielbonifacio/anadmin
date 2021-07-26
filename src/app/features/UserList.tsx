7a5166982e231dcbfcef8b85a1641d4ae6377123
Daniel Bonifacio <contato@danielbonifacio.com.br>
2021-07-25 23:29:16 -0300

    13.43 - Filtro de busca personalizado e reutilizável

--- src/app/features/UserList.tsx
+++ src/app/features/UserList.tsx
@@ -6,6 +6,8 @@ import {
   Tag,
   Typography,
   Avatar,
+  Card,
+  Input,
 } from 'antd';
 import { User } from 'danielbonifacio-sdk';
 import { format } from 'date-fns';
@@ -14,7 +16,9 @@ import useUsers from '../../core/hooks/useUsers';
 import {
   EyeOutlined,
   EditOutlined,
+  SearchOutlined,
 } from '@ant-design/icons';
+import { ColumnProps } from 'antd/lib/table';
 
 export default function UserList() {
   const { users, fetchUsers, toggleUserStatus } =
@@ -24,6 +28,63 @@ export default function UserList() {
     fetchUsers();
   }, [fetchUsers]);
 
+  const getColumnSearchProps = (
+    dataIndex: keyof User.Summary,
+    displayName?: string
+  ): ColumnProps<User.Summary> => ({
+    filterDropdown: ({
+      selectedKeys,
+      setSelectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <Card>
+        <Input
+          style={{ marginBottom: 8, display: 'block' }}
+          value={selectedKeys[0]}
+          placeholder={`Buscar ${displayName || dataIndex}`}
+          onChange={(e) => {
+            setSelectedKeys(
+              e.target.value ? [e.target.value] : []
+            );
+          }}
+          onPressEnter={() => confirm()}
+        />
+        <Space>
+          <Button
+            type={'primary'}
+            size={'small'}
+            style={{ width: 90 }}
+            onClick={() => confirm()}
+            icon={<SearchOutlined />}
+          >
+            Buscar
+          </Button>
+          <Button
+            onClick={clearFilters}
+            size={'small'}
+            style={{ width: 90 }}
+          >
+            Limpar
+          </Button>
+        </Space>
+      </Card>
+    ),
+    filterIcon: (filtered: boolean) => (
+      <SearchOutlined
+        style={{ color: filtered ? '#0099ff' : undefined }}
+      />
+    ),
+    // @ts-ignore
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex]
+            .toString()
+            .toLowerCase()
+            .includes((value as string).toLowerCase())
+        : '',
+  });
+
   return (
     <>
       <Table<User.Summary>
@@ -32,6 +93,7 @@ export default function UserList() {
           {
             dataIndex: 'name',
             title: 'Nome',
+            ...getColumnSearchProps('name', 'nome'),
             width: 160,
             render(name: string, row) {
               return (
@@ -55,6 +117,7 @@ export default function UserList() {
             title: 'Email',
             ellipsis: true,
             width: 240,
+            ...getColumnSearchProps('email', 'Email'),
           },
           {
             dataIndex: 'role',
