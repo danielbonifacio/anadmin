597901683f709e82de43e2cae1931aee192ccd24
Daniel Bonifacio <contato@danielbonifacio.com.br>
2021-07-29 13:04:08 -0300

    14.39 - Desformatando valores antes de enviar para API

--- src/app/features/UserForm.tsx
+++ src/app/features/UserForm.tsx
@@ -346,6 +346,11 @@ export default function UserForm() {
                     <MaskedInput
                       mask='(11) 11111-1111'
                       placeholder={'(27) 99999-0000'}
+                      onChange={event => {
+                        form.setFieldsValue({
+                          phone: event.target.value.replace(/\D/g, '')
+                        })
+                      }}
                     />
                   </Form.Item>
                 </Col>
@@ -366,7 +371,13 @@ export default function UserForm() {
                   >
                     <MaskedInput
                       mask='111.111.111-11'
-                      placeholder={'111.222.333-44'} />
+                      placeholder={'111.222.333-44'}
+                      onChange={event => {
+                        form.setFieldsValue({
+                          taxpayerId: event.target.value.replace(/\D/g, '')
+                        })
+                      }}
+                    />
                   </Form.Item>
                 </Col>
                 <Col lg={8}>
