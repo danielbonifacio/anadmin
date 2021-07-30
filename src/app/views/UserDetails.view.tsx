9d3a1919299eb588fecb4f31c73ddfef6f768508
Daniel Bonifacio <contato@danielbonifacio.com.br>
2021-07-30 03:35:06 -0300

    14.60 - Responsividade com hook useBreakpoint

--- src/app/views/UserDetails.view.tsx
+++ src/app/views/UserDetails.view.tsx
@@ -8,14 +8,17 @@ import {
   Typography,
   Progress,
   Descriptions,
+  Divider,
 } from 'antd';
 import Avatar from 'antd/lib/avatar/avatar';
+import useBreakpoint from 'antd/lib/grid/hooks/useBreakpoint';
 import { useEffect } from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 import useUser from '../../core/hooks/useUser';
 
 export default function UserDetailsView() {
   const params = useParams<{ id: string }>();
+  const { lg } = useBreakpoint();
 
   const { user, fetchUser, notFound } = useUser();
 
@@ -32,22 +35,38 @@ export default function UserDetailsView() {
   if (!user) return <Skeleton />;
 
   return (
-    <Row>
+    <Row gutter={24}>
       <Col xs={24} lg={4}>
-        <Avatar size={120} src={user.avatarUrls.small} />
+        <Row justify={'center'}>
+          <Avatar size={120} src={user.avatarUrls.small} />
+        </Row>
       </Col>
       <Col xs={24} lg={20}>
-        <Typography.Title level={2}>
-          {user.name}
-        </Typography.Title>
-        <Typography.Paragraph ellipsis>
-          {user.bio}
-        </Typography.Paragraph>
-        <Space>
-          <Button type={'primary'}>Editar perfil</Button>
-          <Button type={'primary'}>Remover</Button>
+        <Space
+          style={{ width: '100%' }}
+          direction={'vertical'}
+          align={lg ? 'start' : 'center'}
+        >
+          <Typography.Title level={2}>
+            {user.name}
+          </Typography.Title>
+          <Typography.Paragraph
+            style={{
+              textAlign: lg ? 'left' : 'center',
+            }}
+            ellipsis={{
+              rows: 2,
+            }}
+          >
+            {user.bio}
+          </Typography.Paragraph>
+          <Space>
+            <Button type={'primary'}>Editar perfil</Button>
+            <Button type={'primary'}>Remover</Button>
+          </Space>
         </Space>
       </Col>
+      <Divider />
       <Col xs={24} lg={12}>
         <Space
           direction='vertical'
