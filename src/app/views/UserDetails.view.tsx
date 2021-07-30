b6e8a111be8be22f028dbea8a2392f498a246b04
Daniel Bonifacio <contato@danielbonifacio.com.br>
2021-07-30 04:36:45 -0300

    14.63 - Desafio - Lista de posts

--- src/app/views/UserDetails.view.tsx
+++ src/app/views/UserDetails.view.tsx
@@ -10,6 +10,9 @@ import {
   Descriptions,
   Divider,
   Popconfirm,
+  Table,
+  Switch,
+  Tooltip,
 } from 'antd';
 import Avatar from 'antd/lib/avatar/avatar';
 import useBreakpoint from 'antd/lib/grid/hooks/useBreakpoint';
@@ -22,6 +25,9 @@ import {
 } from 'react-router-dom';
 import useUser from '../../core/hooks/useUser';
 import { WarningFilled } from '@ant-design/icons';
+import moment from 'moment';
+import { Post } from 'danielbonifacio-sdk';
+import usePosts from '../../core/hooks/usePosts';
 export default function UserDetailsView() {
   const params = useParams<{ id: string }>();
   const { lg } = useBreakpoint();
@@ -29,10 +35,22 @@ export default function UserDetailsView() {
   const { user, fetchUser, notFound, toggleUserStatus } =
     useUser();
 
+  const {
+    fetchUserPosts,
+    posts,
+    togglePostStatus,
+    loadingFetch,
+    loadingToggle,
+  } = usePosts();
+
   useEffect(() => {
     if (!isNaN(Number(params.id)))
       fetchUser(Number(params.id));
   }, [fetchUser, params.id]);
+
+  useEffect(() => {
+    if (user?.role === 'EDITOR') fetchUserPosts(user.id);
+  }, [fetchUserPosts, user]);
 
   if (isNaN(Number(params.id)))
     return <Redirect to={'/usuarios'} />;
@@ -143,6 +161,96 @@ export default function UserDetailsView() {
             {user.phone}
           </Descriptions.Item>
         </Descriptions>
+      </Col>
+      <Divider />
+      <Col xs={24}>
+        <Table<Post.Summary>
+          dataSource={posts?.content}
+          rowKey={'id'}
+          loading={loadingFetch}
+          columns={[
+            {
+              responsive: ['xs'],
+              title: 'Posts',
+              render(element) {
+                return (
+                  <Descriptions column={1}>
+                    <Descriptions.Item label={'Título'}>
+                      {element.title}
+                    </Descriptions.Item>
+                    <Descriptions.Item label={'Criação'}>
+                      {moment(element.createdAt).format(
+                        'DD/MM/YYYY'
+                      )}
+                    </Descriptions.Item>
+                    <Descriptions.Item
+                      label={'Atualização'}
+                    >
+                      {moment(element.updatedAt).format(
+                        'DD/MM/YYYY'
+                      )}
+                    </Descriptions.Item>
+                    <Descriptions.Item label={'Publicado'}>
+                      <Switch checked={element.published} />
+                    </Descriptions.Item>
+                  </Descriptions>
+                );
+              },
+            },
+            {
+              dataIndex: 'title',
+              title: 'Título',
+              ellipsis: true,
+              width: 300,
+              responsive: ['sm'],
+              render(title: string) {
+                return (
+                  <Tooltip title={title}>{title}</Tooltip>
+                );
+              },
+            },
+            {
+              dataIndex: 'createdAt',
+              title: 'Criação',
+              width: 180,
+              align: 'center',
+              responsive: ['sm'],
+              render: (item) =>
+                moment(item).format('DD/MM/YYYY'),
+            },
+            {
+              dataIndex: 'updatedAt',
+              title: 'Última atualização',
+              width: 200,
+              align: 'center',
+              responsive: ['sm'],
+              render: (item) =>
+                moment(item).format(
+                  'DD/MM/YYYY \\à\\s hh:mm'
+                ),
+            },
+            {
+              dataIndex: 'published',
+              title: 'Publicado',
+              align: 'center',
+              width: 120,
+              responsive: ['sm'],
+              render(published: boolean, post) {
+                return (
+                  <Switch
+                    checked={published}
+                    loading={loadingToggle}
+                    onChange={() => {
+                      togglePostStatus(post).then(() => {
+                        fetchUserPosts(user.id);
+                      });
+                    }}
+                  />
+                );
+              },
+            },
+          ]}
+        />
       </Col>
     </Row>
   );
